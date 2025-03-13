e6c4a74a98965dff7dbe6f25228f4801eb9ba69e
arnie00782 <50872497+arnie00782@users.noreply.github.com>
2025-03-13 22:53:21 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,35 @@
+#include "led-matrix.h"
+#include "retro_matrix.h"
+#include "pacman.h"
 
+int main(int argc, char* argv[]) {
+    RGBMatrix::Options defaults;
+    defaults.rows = 64;
+    defaults.cols = 64;
+    defaults.chain_length = 1;
+    Canvas* canvas = rgb_matrix::CreateMatrixFromFlags(&argc, &argv, &defaults);
+    if (canvas == NULL) return 1;
+
+    cout << "Wähle ein Spielmodus:\n";
+    cout << "1. Standard-Modus\n";
+    cout << "2. Pac-Man\n";
+    
+    int choice;
+    cin >> choice;
+
+    if (choice == 2) {
+        PacManGame game;
+        char input;
+        while (game.running) {
+            cin >> input; 
+            game.update(input);
+            drawGame(canvas, game);
+            this_thread::sleep_for(chrono::milliseconds(200));
+        }
+    } else {
+        cout << "Starte Standard-Modus...\n";
+    }
+
+    delete canvas;
+    return 0;
+}
